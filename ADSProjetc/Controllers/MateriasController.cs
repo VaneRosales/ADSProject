b8f114276e71d2bfedb9f9217a99bb638ecc9f6d
Vane Rosales <128653857+VaneRosales@users.noreply.github.com>
2024-04-12 23:43:52 -0600

    Se agregaron validaciones para el modelo de Materias

--- ADSProjetc/Controllers/MateriasController.cs
+++ ADSProjetc/Controllers/MateriasController.cs
@@ -23,6 +23,10 @@ namespace ADSProjetc.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.AgregarMateria(materia);
                 if (contador > 0)
                 {
@@ -51,6 +55,10 @@ namespace ADSProjetc.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.ActualizarMateria(idMateria, materia);
                 if (contador > 0)
                 {
--- ADSProjetc/Models/Materia.cs
+++ ADSProjetc/Models/Materia.cs
@@ -1,8 +1,12 @@
-﻿namespace ADSProjetc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADSProjetc.Models
 {
     public class Materia
     {
         public int IdMateria { get; set; }
+        [Required(ErrorMessage = "Este es un campo requerido")]
+        [MaxLength(length: 50, ErrorMessage = "La longitud del campo no puede ser mayor a 50 caracteres.")]
         public string NombreMateria { get; set; }
     }
 }
